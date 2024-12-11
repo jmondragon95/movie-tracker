9031b4ad4cf70d8f45aec6ab0107edbf5b9e6ebd
Kelia <smith.kelia93@gmail.com>
2024-12-10 18:15:17 -0800

    fixed search button and search bar width

--- index.mjs
+++ index.mjs
@@ -1,14 +1,23 @@
 //  Imports
+
 import express from "express";
 import mysql from "mysql2/promise";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import configDotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import crypto from "crypto";
+
 
 //  Global Variables
+
+configDotenv.config();  //  allows us to use .env file variables
 const app = express();
 const pool = mysql.createPool({
-  host: "jmondragon.tech",
-  user: "jmondrag_movieWatcher",
-  password: "CSUMB-cst336@",
-  database: "jmondrag_movies",
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   connectionLimit: 10,
   waitForConnections: true,
 });
@@ -17,25 +26,319 @@ let apiKey = "683b5907";
 let page = 1;
 
 //  Setup view engine and public (static) directory
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));  //  Allows us to read body from post requests
+app.use(express.json());  //  Allows routes to read json
+app.use(cookieParser());  //  Allows read/write of cookies
 
-app.get("/", (req, res) => {
-  res.render("index");
+//  Functions
+
+async function authenticateToken(req, res, next) {
+  //  RETRIEVE COOKIE FROM REQUEST COOKIES
+  const accessToken = req.cookies.accessToken;
+  if (accessToken) {
+    //  ACCESS TOKEN IS FOUND, LETS TRY VERIFYING IT
+    try {
+      // IF VERIFICATION SUCCEEDS (NOT EXPIRED AND IS VALID, GO NEXT
+      const accessTokenPayload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const userId = accessTokenPayload.userId;
+      req.userId = userId;
+      next();
+    } catch (error) {
+      //  IF TOKEN IS EXPIRED, THEN USE REFRESH TOKEN TO RETRIEVE NEW (ACCESS AND REFRESH) TOKENS
+      if (error.name === 'TokenExpiredError') {
+        //  RETRIEVE REFRESH TOKEN FROM REQUEST COOKIES
+        const refreshToken = req.cookies.refreshToken;
+        if (refreshToken) {
+          //  REFRESH TOKEN IS FOUND, LETS TRY VERIFYING IT (NOT USED AND IS VALID)
+          try {
+            //  VERIFIES REFRESH TOKEN AND STORES PAYLOAD IN VARIABLE
+            const refreshTokenPayload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+            //  LET US VERIFY THAT THIS REFRESH TOKEN HASN'T BEEN USED ALREADY.
+            if (await verifyRefreshToken(refreshToken)) {
+              const userId = refreshTokenPayload.userId;
+              //  RETURNS ARRAY [ACCESS TOKEN, REFRESH TOKEN]
+              const newTokens = await generateNewTokens(userId);
+              const newAccessToken = newTokens[0];
+              const newRefreshToken = newTokens[1];
+              //  SETTING ACCESS/REFRESH TOKEN IN COOKIES
+              res.cookie('accessToken', newAccessToken, {
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60
+              });
+              res.cookie('refreshToken', newRefreshToken, {
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 24 * 30
+              });
+              req.userId = userId;
+              return next();
+            } else {
+              console.log('line 68: verifyRefreshToken returned false');
+            }
+          } catch (error) {
+            console.log('Error occurred while verifying refresh token', error);
+          }
+        } else {
+          //  IF REFRESH TOKEN IS NOT FOUND, REDIRECT TO LOGIN PAGE
+          console.log('No refresh token');
+        }
+
+      } else {
+        console.log('error with token verification:', error);
+      }
+      console.log('clearing cookies');
+      //  CLEARING COOKIES
+      res.clearCookie('accessToken', {httpOnly: true});
+      res.clearCookie('refreshToken', {httpOnly: true});
+      return res.redirect('/login');
+    }
+  } else {
+    //  NO ACCESS TOKEN
+    console.log('No access token');
+    return res.redirect('/login');
+  }
+}
+
+async function verifyRefreshToken(refreshToken) {
+  //  HASH THE REFRESH TOKEN
+  const hashedRefreshToken = crypto.createHash('sha256').update(refreshToken).digest('hex');
+  //  SQL TO FIND REFRESH TOKEN IN DATABASE
+  const retrieveRefreshTokenSQL = 'SELECT * FROM refresh_tokens WHERE refresh_token = ? AND is_used = FALSE';
+  try {
+    const [refreshTokenFromDB] = await sqlConnection.query(retrieveRefreshTokenSQL, [hashedRefreshToken]);
+    console.log('line 91:', refreshTokenFromDB);
+    if (refreshTokenFromDB.length > 0) {
+      //  SET REFRESH TOKEN AS USED IN DATABASE
+      const setRefreshTokenUsedSQL = 'UPDATE refresh_tokens SET is_used = TRUE WHERE refresh_token = ?';
+      try {
+        console.log('line 100: updating refresh token in db', hashedRefreshToken);
+        await sqlConnection.query(setRefreshTokenUsedSQL, [hashedRefreshToken]);
+        return true;
+      } catch (error) {
+        console.log('line 102', error);
+        return false;
+      }
+    } else {
+      console.log('line 107: refreshToken not found in db');
+      return false;
+    }
+  } catch (error) {
+    console.log('line 106', error);
+    return false;
+  }
+}
+
+async function generateNewTokens(userId) {
+  //  GENERATE USER OBJECT TO STORE AS PAYLOAD IN TOKENS
+  const userObject = {'userId': userId};
+  //  GENERATE NEW ACCESS/REFRESH TOKENS
+  const newAccessToken = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'});
+  const newRefreshToken = jwt.sign(userObject, process.env.REFRESH_TOKEN_SECRET);
+  //  STORE NEW REFRESH TOKEN IN DATABASE
+  const hashedRefreshToken = crypto.createHash('sha256').update(newRefreshToken).digest('hex');
+  const insertRefreshTokenSQL = 'INSERT INTO refresh_tokens (user_id, refresh_token) ' +
+      'VALUES (?, ?)';
+  try {
+    await sqlConnection.query(insertRefreshTokenSQL, [userId, hashedRefreshToken]);
+  } catch (error) {
+    console.log('generateNewTokens, Error occurred while inserting hashed refresh token:', error);
+    return [];
+  }
+  //  STORE IN ARRAY TO RETURN TO AUTHENTICATE TOKENS
+  const newTokens = [];
+  newTokens.push(newAccessToken);
+  newTokens.push(newRefreshToken);
+  return newTokens;
+}
+
+//  Get Routes
+
+app.get("/", authenticateToken, (req, res) => {
+  const userId = req.userId;
+  const { message, border } = req.query;
+  res.render("index", {message, border, userId});
+});
+
+//route to render movies by keywords using the search bar
+app.get("/searchResults", authenticateToken, async (req, res) => {
+  const userId = req.userId;
+  const { message, border } = req.query;
+  let keyword = req.query.search;
+  let sql = `SELECT *
+              FROM movies
+              WHERE title LIKE ?`;
+  let sqlParams = [`%${keyword}%`];
+  const [rows] = await mySQLConnection.query(sql, sqlParams);
+  console.log(rows);
+  res.render("searchResults", {"searchMovies": rows, keyword, message, border, userId});
 });
 
 app.get("/trending", (req, res) => {
   res.render("trending");
 });
 
-app.get("/watchlist", (req, res) => {
-  res.render("watchlist");
+
+//route to watchlist page will render movie poster and title
+//also has model to display rest of info
+app.get("/watchlist", authenticateToken, async (req, res) => {
+  const userId = req.userId;
+  const { message, border } = req.query;
+  let sql = `SELECT movie_id, poster_url, title
+              FROM watchlists
+              NATURAL JOIN movies 
+              WHERE user_id = ?`
+  const [rows] = await mySQLConnection.query(sql, userId);
+  console.log(rows);
+  res.render("watchlist", {"watchlistMovies": rows, message, border, userId});
 });
 
-app.get("/favorites", (req, res) => {
-  res.render("favorites");
+//route to add movies from index to watchlist
+app.post("/addTowatchlist", authenticateToken, async function(req, res) {
+  const userId = req.userId;
+  let movie_id = req.body.btnAddWatchlist;
+  let sqlCheck = `SELECT *
+                  FROM watchlists
+                  WHERE movie_id = ?`;
+  const[rowsCheck] = await mySQLConnection.query(sqlCheck, movie_id);
+  console.log(rowsCheck);
+  if(rowsCheck.length > 0){
+    res.render("index", {"message": "Movie Already In Watchlist!"})
+  }else{
+    let sql = `INSERT INTO watchlists 
+              (user_id, movie_id)
+              VALUES (?, ?)`; 
+    let params = [userId, movie_id];
+    const [rows] = await mySQLConnection.query(sql, params);
+    res.render("index", {"message": "Movie Added To Watchlist!"})
+  }
 });
 
+//route to favorites page will render movie poster and title
+//also has model to display rest of info
+app.get("/favorites", authenticateToken, async(req, res) => {
+  const userId = req.userId;
+  const { message, border } = req.query;
+  let sql = `SELECT movie_id, poster_url, title
+              FROM favorites
+              NATURAL JOIN movies 
+              WHERE user_id = ?`
+  const [rows] = await mySQLConnection.query(sql, userId);
+  console.log(rows);
+  res.render("favorites", {"favMovies": rows, message, border, userId});
+});
+
+//route to add movies to favorites from index
+app.post("/addToFavorites", authenticateToken, async function(req, res) {
+  const userId = req.userId;
+  let movie_id = req.body.btnAddFavorite;
+  let sqlCheck = `SELECT *
+                  FROM favorites
+                  WHERE movie_id = ?`;
+  const[rowsCheck] = await mySQLConnection.query(sqlCheck, movie_id);
+  console.log(rowsCheck);
+  if(rowsCheck.length > 0){
+    res.render("index", {"message": "Movie Already In Favorites!"});
+  }else{
+    let sql = `INSERT INTO favorites 
+              (user_id, movie_id)
+              VALUES (?, ?)`;  
+    let params = [userId, movie_id];
+    const [rows] = await mySQLConnection.query(sql, params);
+    res.render("index", {"message": "Movie Added To Favorites!"});
+  }
+});
+
+//  Login routes
+
+app.get('/login', (req, res) => {
+  const accessToken = req.cookies.accessToken;
+  if (accessToken) {
+    res.redirect('/');
+  } else {
+    const { message, border } = req.query;
+    res.render('login', {message, border});
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const findUserSQL = 'SELECT * FROM users WHERE username = ?';
+  try {
+    const [userRow] = await mySQLConnection.query(findUserSQL, [username]);
+    const user = userRow[0];
+    const hashedPassword = user.hashed_password;
+    const match = await bcrypt.compare(password, hashedPassword);
+    if (match) {
+      const userId = user.user_id;
+      const userObject = {'userId': userId};
+      const accessToken = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      const refreshToken = jwt.sign(userObject, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '30d'});
+      const hashedRefreshToken = crypto.createHash('sha256').update(refreshToken).digest('hex');
+      const insertRefreshTokenSQL = 'INSERT INTO refresh_tokens (user_id, refresh_token) ' +
+          'VALUES (?, ?)';
+      try {
+        await mySQLConnection.query(insertRefreshTokenSQL, [userId, hashedRefreshToken]);
+      } catch (error) {
+        console.log('Error occurred while inserting refresh token to database', error);
+        return res.redirect(`/login?message=${error}&border=text-bg-warning`);
+      }
+      res.cookie('accessToken', accessToken, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60
+      });
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 30
+      });
+      return res.redirect('/?message=Login successful&border=text-bg-success');
+    } else {
+      return res.redirect('/login?message=Incorrect password&border=text-bg-danger');
+    }
+  } catch (error) {
+    console.log('Error occurred while retrieving user from database', error);
+    return res.redirect(`/login?message=Incorrect username&border=text-bg-warning`);
+  }
+});
+
+app.get('/logout', (req, res) => {
+  console.log('clearing cookies');
+  //  CLEARING COOKIES
+  res.clearCookie('accessToken', {httpOnly: true});
+  res.clearCookie('refreshToken', {httpOnly: true});
+  res.redirect('/login');
+});
+
+app.get('/register', (req, res) => {
+  const { message, border } = req.query;
+  res.render('register', {message, border});
+});
+
+app.post('/register', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const confirmedPassword = req.body.confirmPassword;
+  console.log(password, confirmedPassword);
+  if (password === confirmedPassword) {
+    console.log('passwords match');
+    //  Hash password: prepare for insertion into database
+    const hashedPassword = await bcrypt.hash(password, 10);
+    //  Set up insert sql statement and parameters
+    const insertUserSQL = 'INSERT INTO users (username, hashed_password) VALUES (?, ?)';
+    const params = [username, hashedPassword];
+    try {
+      await mySQLConnection.query(insertUserSQL, params);
+      return res.redirect('/login?message=Registration successful&border=text-bg-success');
+    } catch (error) {
+      console.log('Error occurred while inserting user to database', error);
+      return res.redirect(`/register?message=${error}&border=text-bg-danger`);
+    }
+  } else {
+    return res.redirect('/register?message=Passwords do not match&border=text-bg-warning');
+  }
+});
 
 //  Retrieves movies from API and stores in database
 app.get("/movies", async (req, res) => {
@@ -53,7 +356,7 @@ app.get("/movies", async (req, res) => {
 
   if (movieRows.length === 0) {
     while (movieRows.length === 0) {
-      console.log("Page:", page);
+    //  console.log("Page:", page);
       // console.log("No movies found in jmondrag_movies");
       //  Retrieve first set of movies from API
       let movieSetUrl = `https://www.omdbapi.com/?apikey=${apiKey}&s=Batman&type=movie&page=${page}`;
@@ -63,7 +366,7 @@ app.get("/movies", async (req, res) => {
       if (movieSetData.Search) {
         let count = 1;
         for (let movie of movieSetData.Search) {
-          console.log("movie:", count);
+        //  console.log("movie:", count);
           //  Retrieve individual movie information
           let movieUrl = `https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`;
           let movieResponse = await fetch(movieUrl);
@@ -79,7 +382,7 @@ app.get("/movies", async (req, res) => {
           let metacriticRating =
             movieData.Ratings.find((r) => r.Source === "Metacritic")?.Value ||
             null;
-          console.log(movieData.Title, movieData.Released);
+         // console.log(movieData.Title, movieData.Released);
           let releaseDate;
           if (movieData.Released === "N/A") {
             releaseDate = null;
@@ -95,8 +398,14 @@ app.get("/movies", async (req, res) => {
             "movie_id, title, actors, genre, runtime, age_rating, imdb_rating," +
             "rotten_tomatoes_rating, metacritic_rating, poster_url, release_date, director, " +
             "description) " +
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
-
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" +
+            "ON DUPLICATE KEY UPDATE " +
+            "title = VALUES(title), actors = VALUES(actors), genre = VALUES(genre), " +
+            "runtime = VALUES(runtime), age_rating = VALUES(age_rating), imdb_rating = VALUES(imdb_rating), " +
+            "rotten_tomatoes_rating = VALUES(rotten_tomatoes_rating), metacritic_rating = VALUES(metacritic_rating), " +
+            "poster_url = VALUES(poster_url), release_date = VALUES(release_date), director = VALUES(director), " +
+            "description = VALUES(description);";
+      
           await mySQLConnection.execute(insertMovieSQL, [
             movieData.imdbID,
             movieData.Title,
@@ -119,15 +428,27 @@ app.get("/movies", async (req, res) => {
         break;
       }
       page++;
+
       //  Retry search from database now that it is populated.
-      let moviesResult = await mySQLConnection.query(movieSearchSQL, [
-        lastMovieID,
+      moviesResult = await mySQLConnection.query(movieSearchSQL, [
         limit,
+        offset,     
       ]);
       movieRows = moviesResult[0];
     }
   }
   res.json(movieRows);
+});
+
+//local api to display movie info on modal
+//sending data in JSON format
+app.get('/api/movies/:id', async (req, res) => {
+  let movieId = req.params.id;
+  let sql = `SELECT *
+            FROM movies
+            WHERE movie_id = ?`;           
+  let [rows] = await mySQLConnection.query(sql, [movieId]);
+  res.send(rows)
 });
 
 app.listen(3000, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,25 @@
+
+//  BOOTSTRAP VALIDATION FUNCTION
+
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
 //  Event Listeners
 
 window.addEventListener("scroll", () => {
@@ -5,6 +27,10 @@ window.addEventListener("scroll", () => {
     fetchMovies();
   }
 });
+
+document.addEventListener('DOMContentLoaded', showToast);
+//adde event listener for watchlist and favorites
+addMovieEventListeners();
 
 // Global Variables
 
@@ -15,13 +41,26 @@ const moviesContainer = document.getElementById("movies-container");
 
 const storedMovies = JSON.parse(localStorage.getItem("movies")) || [];
 
-if (storedMovies.length > 0) {
+if (storedMovies.length > 0 && moviesContainer) {
   renderMovies(storedMovies);
 } else {
   fetchMovies();
 }
 
 //  Functions
+
+function showToast() {
+  const toastElement = document.getElementById('toastMessage');
+  //  If toast element is found on page, continue
+  if (toastElement) {
+    const toastMessageElement = document.getElementById('toastMessage');
+    //  If toast message has text inside, show it
+    if (toastMessageElement.textContent.trim() !== '') {
+      const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastElement);
+      toastBootstrap.show();
+    }
+  }
+}
 
 async function fetchMovies() {
   //  If we are loading, cancel this extra fetch function
@@ -37,11 +76,14 @@ async function fetchMovies() {
       //  Update offset and store locally
       offset += movies.length;
       localStorage.setItem("offset", offset);
+
       //  Merge new movies with stored ones and save to local storage
       const updatedMovies = [...storedMovies, ...movies];
       localStorage.setItem("movies", JSON.stringify(updatedMovies));
       //  Render new movies
-      renderMovies(movies);
+      if (moviesContainer) {
+        renderMovies(movies);
+      }
     } else {
       console.log("No more movies available.");
     }
@@ -53,6 +95,8 @@ async function fetchMovies() {
 }
 
 function renderMovies(movies) {
+
+  //render movies
   movies.forEach((movie) => {
     const movieCard = document.createElement("div");
     movieCard.className = "card text-bg-secondary";
@@ -61,17 +105,60 @@ function renderMovies(movies) {
     } else {
       movieCard.innerHTML = `<img src="${movie.poster_url}" alt="${movie.title}">`;
     }
-    if (movie.description === "N/A") {
-      movieCard.innerHTML += `<div class="card-body">
-                  <h3 class="card-title">${movie.title}</h3>
-                  <p class="card-text">Description unavailable</p>
-                                  </div>`;
-    } else {
-      movieCard.innerHTML += `<div class="card-body">
-                <h3 class="card-title">${movie.title}</h3>
-                <p class="card-text">${movie.description}</p>
-            </div>`;
-    }
+
+   //make movie titles clickable
+    movieCard.innerHTML += `<a href="#" class="movieAnchor" id="${movie.movie_id}">${movie.title}</a>`;
     moviesContainer.appendChild(movieCard);
+
   });
+  //add event listener everytime new movies are rendered.
+  addMovieEventListeners();
 }
+
+// Event listener function to add to the movie anchors
+function addMovieEventListeners(){
+
+  const movieInfoLinks = document.querySelectorAll(".movieAnchor");
+  for (let movieInfoLink of movieInfoLinks) {
+    movieInfoLink.addEventListener("click", getMovieInfo);
+  }
+}
+
+
+//function to display movie info on modal
+async function getMovieInfo() { 
+  console.log(this.id);
+  var myModal = new bootstrap.Modal(document.getElementById('movieModal'));
+  myModal.show();
+  let url = `/api/movies/${this.id}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+
+  //convert date
+  const releasedOn = new Date(data[0].release_date);
+
+  //display api data for movie modal
+  let movieInfo = document.querySelector("#movieInfo");
+  movieInfo.innerHTML = `<h1> ${data[0].title}</h1>`;
+  movieInfo.innerHTML += `<img src="${data[0].poster_url}" alt="${data[0].poster_url}" width="auto"><br><br>`;
+  movieInfo.innerHTML += `<div><strong>Rated: </strong>${data[0].age_rating} </div>`;
+  movieInfo.innerHTML += `<div><strong>Actors: </strong>${data[0].actors} </div>`;
+  movieInfo.innerHTML += `<div><strong>Runtime: </strong>${data[0].runtime} </div>`;
+  movieInfo.innerHTML += `<div><strong>Released Date: </strong>${releasedOn.toLocaleDateString()} </div>`;
+  movieInfo.innerHTML += `<div><strong>Director: </strong>${data[0].director} </div>`;
+  movieInfo.innerHTML += `<div><strong>Description: </strong>${data[0].description} </div>`;
+  movieInfo.innerHTML += `<div><strong>IMDb-Rating: </strong>${data[0].imdb_rating} </div>`;
+  movieInfo.innerHTML += `<div><strong>Rotten-Tomatoes-Rating: </strong>${data[0].rotten_tomatoes_rating} </div>`;
+  let modalFooter = document.querySelector(".modal-footer");
+  modalFooter.innerHTML = `<form method="POST" action="/rateMovie">
+                              <button type="submit" name="btnRateMovie"  id="btnRateMovie" value="${data[0].movie_id}" >Rate</button>
+                              </form>`;
+  modalFooter.innerHTML +=`<form method="POST" action="/addToWatchlist">
+                              <button type="submit" name="btnAddWatchlist" id="btnAddWatchList" value="${data[0].movie_id}" >Add to Watchlist</button>
+                              </form>`;
+  modalFooter.innerHTML +=`<form method="POST" action="/addToFavorites">
+                            <button type="submit" name="btnAddFavorite" id="btnAddFavorite" value="${data[0].movie_id}" >Add to Favorites</button>
+                            </form>`;
+}
+
